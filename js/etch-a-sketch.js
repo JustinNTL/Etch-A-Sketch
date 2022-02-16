57a7b6d9be9824957ffcbd6104f28cc861b7b310
Justin <Justin.nthai@gmail.com>
2022-02-16 14:34:27 -0800

    Replace className for classList on panel for more suitable use case, Add event for colorPanel in js and edit in css

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,6 +1,16 @@
 const gridPanels = document.querySelector('.gridPanels');
+
+generateGrid = () => {
 for (let i=0; i<256; i++) {
   const panel = document.createElement('div');
-  panel.className = 'panel';
+  panel.classList = 'panel';
   gridPanels.appendChild(panel);
-}
+  }
+};
+
+const panel = document.querySelector('div');
+panel.addEventListener('mouseover', function(e) {
+  e.target.classList.replace('panel', 'colorPanel');
+});
+
+generateGrid();

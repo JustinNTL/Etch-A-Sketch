a5400dc0c21b771c1ebbea81641a0eccae8247cb
Justin <Justin.nthai@gmail.com>
2022-02-17 14:03:07 -0800

    Alter generateGrid function to declaration using default column and row arguments to be called now in updateGrid function, Update grid dimensions using user entry to adjust the grid css properties

--- js/etch-a-sketch.js
+++ js/etch-a-sketch.js
@@ -1,12 +1,14 @@
 const gridPanels = document.querySelector('.gridPanels');
+const defaultColumn = 16;
+const defualtRow = 16;
 
-generateGrid = () => {
-for (let i=0; i<256; i++) {
+function generateGrid(defaultColumn, defualtRow) {
+for (let i=0; i< (defaultColumn * defualtRow); i++) {
   const panel = document.createElement('div');
   panel.classList = 'panel';
   gridPanels.appendChild(panel);
   }
-};
+}
 
 gridPanels.addEventListener('mouseover', function(e) {
   e.target.classList.replace('panel', 'colorPanel');
@@ -19,10 +21,17 @@ updateGrid = () => {
     alert(`${panelNum} is not a valid entry, please try again.`);
     panelNum = prompt('Please specify new grid dimensions between 2-100: ');
   }
-}
+  gridPanels.style.setProperty(
+    'grid-template-columns', `repeat(${panelNum}, 2fr)`
+  );
+  gridPanels.style.setProperty(
+    'grid-template-rows', `repeat(${panelNum}, 2fr)`
+  );
+  generateGrid(panelNum, panelNum);
+};
 
 const resetBtn = document.querySelector('.resetBtn');
 resetBtn.addEventListener('click', updateGrid);
 
-generateGrid();
+generateGrid(defaultColumn, defualtRow);
 
